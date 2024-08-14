d6fbf68eb0b963e8cc7dc4cb579ccb7b9637602c
Hazem Fady <hazemfady123@gmail.com>
2024-08-14 08:58:12 +0300

    Add project files.

---
+++ LinQ_C#_02_Demo/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace LinQ_C__02_Demo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
