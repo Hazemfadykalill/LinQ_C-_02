5fbcd037bbaf8debd41a5771ab54c251e1e91263
Hazem Fady <hazemfady123@gmail.com>
2024-08-14 10:37:27 +0300

    1. Category : Filtration [Restriction] Where && Indexed Where

--- LinQ_C#_02_Demo/Program.cs
+++ LinQ_C#_02_Demo/Program.cs
@@ -4,7 +4,58 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine(ListGenerator.ProductsList[0]);
+            #region Filtration [Restriction]
+            #region Where
+            #region Get Product out of stock
+            //get product out of stock
+            //By Use Fluent Syntax
+            //var Result = ListGenerator.ProductsList.Where(P => P.UnitsInStock == 0);
+
+            ////By Use Query Syntax
+            // Result =from P in ListGenerator.ProductsList
+            //         where P.UnitsInStock==0
+            //         select P;
+
+            //foreach (var product in Result)
+            //{
+            //    Console.WriteLine(product); 
+
+            //} 
+            #endregion
+
+            #region Get product in stock and category  Of Meat/Poultry 
+
+
+            ////By Use Fluent Syntax
+            //var Result = ListGenerator.ProductsList.Where(P => P.UnitsInStock > 0 && P.Category== "Meat/Poultry");
+
+            ////By Use Query Syntax
+            //Result = from P in ListGenerator.ProductsList
+            //         where P.UnitsInStock > 0 && P.Category == "Meat/Poultry"
+            //         select P;
+
+            //foreach (var product in Result)
+            //{
+            //    Console.WriteLine(product);
+
+            //}
+            #endregion
+            #endregion
+
+            #region Indexed Where
+            //Note This use only with fluent syntax
+            //By Use Fluent Syntax
+            //var Result = ListGenerator.ProductsList.Where((P,Index )=> P.UnitsInStock > 0 && Index>10);
+
+           
+
+            //foreach (var product in Result)
+            //{
+            //    Console.WriteLine(product);
+
+            //}
+            #endregion
+            #endregion
         }
     }
 }
