1cb5ab122a942a132b0a238dae4773dac891dca7
Hazem Fady <hazemfady123@gmail.com>
2024-08-15 16:32:11 +0300

    this is solve to LINQ - Element Operators

--- LINQ_C3_02_Assignment/Program.cs
+++ LINQ_C3_02_Assignment/Program.cs
@@ -1,4 +1,7 @@
 ﻿using LinQ_C3_02_Assignment;
+using static System.Runtime.InteropServices.JavaScript.JSType;
+using System.Diagnostics;
+using System.Xml.Linq;
 
 namespace LINQ_C3_02_Assignment
 {
@@ -26,22 +29,47 @@ namespace LINQ_C3_02_Assignment
             #endregion
 
             #region // 3.Returns digits whose name is shorter than their value.
-            
-            string[] Arr =new string[] { "zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine" };
 
-            var Result = Arr.Where((Number,Index)=>Number.Length<Index);
-            foreach (var Number in Result)
-            {
-                Console.Write($"  {Number}");                
-            }
+            //string[] Arr =new string[] { "zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine" };
 
-            #endregion
-
-
+            //var Result = Arr.Where((Number,Index)=>Number.Length<Index);
+            //foreach (var Number in Result)
+            //{
+            //    Console.Write($"  {Number}");                
+            //}
 
             #endregion
 
+
+
+            #endregion
+
+            #region LINQ - Element Operators
+
+
+            #region //1.Get first Product out of Stock
+            //var Result = ListGenerators.ProductList.Where(P => P.UnitsInStock == 0)
+            //                               .First();
+            //Console.WriteLine(Result);
+            #endregion
+            #region //2.Return the first product whose Price > 1000, unless there is no match, in which case null is returned.
+
+            //var Result = ListGenerators.ProductList.FirstOrDefault(P=>P.UnitPrice>1000);
+            //Console.WriteLine(Result); 
+            #endregion
+
+            #region //3.Retrieve the second number greater than 5
+
+            //int[] Arr = { 5, 4, 1, 3, 9, 8, 6, 7, 2, 0 };
+
+            //int[] Result = Arr.Where(N => N > 5).ToArray();
+        
+            //Console.WriteLine($" the second number greater than 5 : {Result[1]} ");
+            #endregion
+
+
+            #endregion
 
         }
-    }
+        }
 }
