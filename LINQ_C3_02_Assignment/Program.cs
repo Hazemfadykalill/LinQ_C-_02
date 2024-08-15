6555d1829c313886dc788acecf1727f8e85f0547
Hazem Fady <hazemfady123@gmail.com>
2024-08-15 15:54:22 +0300

    this Is Start IN Assignment

---
+++ LINQ_C3_02_Assignment/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace LINQ_C3_02_Assignment
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
